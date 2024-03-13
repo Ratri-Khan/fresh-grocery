1d989efa2c0de4ab0ea0f0b7f6a2a10a442978c6
ratrikhanam000 <ratrikhanam000@gmail.com>
2024-03-13 15:05:50 +0600

    category section cover setup complete

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -9,76 +9,82 @@ import img2 from "../../assets/category/slide2.jpg";
 import img3 from "../../assets/category/slide3.jpg";
 import img4 from "../../assets/category/slide4.jpg";
 import img5 from "../../assets/category/slide5.jpg";
-import vegCover from "../../assets/category/vegCover.jpg"
+import vegCover from "../../assets/category/vegCover.jpg";
+import fishCover from "../../assets/category/fishCover.jpg";
+import dalCover from "../../assets/category/dalCover.jpg";
+import fruitsCover from "../../assets/category/fruitsCover.jpg";
 import UseCategory from "../../Hooks/UseCategory";
+import CategoryCover from "../shared/CategoryCover";
 
 const Category = () => {
   const [category] = UseCategory();
-  const vegetables = category.filter(item => item.category === "vegetable");
+  const vegetables = category.filter((item) => item.category === "vegetable");
+  const fishes = category.filter((item) => item.category === "fish");
+  const dal = category.filter((item) => item.category === "dal");
+  const fruits = category.filter((item) => item.category === "vegetable");
   return (
     <div>
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={10}
-        pagination={{
-          clickable: true,
-        }}
-        breakpoints={{
-          640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 4,
-            spaceBetween: 40,
-          },
-          1024: {
-            slidesPerView: 5,
-            spaceBetween: 50,
-          },
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <img src={img1} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={img2} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={img3} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={img4} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={img5} alt="" />
-        </SwiperSlide>
-      </Swiper>
-      <div
-        className="hero min-h-screen"
-        style={{
-          backgroundImage:
-            `url(${vegCover})`,
-        }}
-      >
-        <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <div>
-              {
-                vegetables.map(vegetable => 
-                <div key={vegetable.id}>
-                  <p>{vegetable.name}</p>
-                  <p>{vegetable.price}</p>
-                </div>)
-              }
-            </div>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
-        </div>
+      <div className=" py-20">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={10}
+          pagination={{
+            clickable: true,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 4,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 5,
+              spaceBetween: 50,
+            },
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img src={img1} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={img2} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={img3} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={img4} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={img5} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={img3} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={img4} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={img5} alt="" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div>
+        <CategoryCover data={vegetables} img={vegCover} title="vegetable"></CategoryCover>
+      </div>
+      <div>
+        <CategoryCover data={fishes} img={fishCover} title="fish"></CategoryCover>
+      </div>
+      <div>
+        <CategoryCover data={dal} img={dalCover} title="Dal"></CategoryCover>
+      </div>
+      <div>
+        <CategoryCover data={fruits} img={fruitsCover} title="fruits"></CategoryCover>
       </div>
     </div>
   );
